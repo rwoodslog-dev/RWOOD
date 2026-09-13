99df54a7afe53f04cebe62ed397337d970bf881b
rwoodslog-dev <rwoods.log@gmail.com>
2026-09-13 10:48:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rwood-cache-v757';
+const CACHE_NAME = 'rwood-cache-v759';
 const ASSETS = [
   './manifest.json',
   './icons/icon-192.png',
@@ -27,6 +27,7 @@ const ASSETS = [
   './icons/role-compta.png',
   './icons/role-patron.png',
   './icons/megaphone.png',
+  './icons/messagerie.png',
   './icons/engrenage.png',
   './icons/monnaie.png',
   './icons/ordinateur.png',
